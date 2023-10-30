acf803a5f9c45dfa81f025ccd77b47b84bc7ada9
Mazen Tamer Salah <mazentamer3056@gmail.com>
2023-10-30 06:37:36 +0200

    2 step verification script

---
+++ verification.py
@@ -0,0 +1,15 @@
+import tempmail
+from bs4 import BeautifulSoup
+import json
+
+
+while True:
+    email = input("email: ")
+    login = email.split("@")[0]
+    domain = email.split("@")[1]
+    mail = tempmail.TempMail(login=login, domain=domain)
+    html = mail.get_single_email()['body']
+    soup = BeautifulSoup(html, 'html.parser')
+    strong_elements = soup.find_all('strong')
+    verification_code = strong_elements[2].text if len(strong_elements) > 1 else None
+    print("Verification Code:", verification_code)
